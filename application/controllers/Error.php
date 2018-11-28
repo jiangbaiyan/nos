9035263882d4b43f9b707d235e514e80e750231f
jiangbaiyan <987082332@qq.com>
2018-11-28 13:49:58 +0800

    完成日志处理类

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -9,11 +9,28 @@
 
 class ErrorController extends Comm_Control{
 
+    /**
+     * 异常处理
+     * @param $exception
+     */
     public function errorAction($exception){
         $code = $exception->getCode();
         $msg = $exception->getMessage();
+        $file = $exception->getFile();
+        $line = $exception->getLine();
+        $scheme = 'http';
+        if ($_SERVER['HTTPS'] == 'on'){
+            $scheme .= 's';
+        }
+        Comm_Log::fatal(array(
+            'status' => $code,
+            'msg'    => $msg,
+            'file'   => $file,
+            'line'   => $line,
+            'url'    => $scheme . '://' . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
+            'method' => $_SERVER['REQUEST_METHOD'],
+        ));
         $this->response($code,$msg);
-        Comm_Log::fatal();//写日志
     }
 
 }
--- application/library/comm/Log.php
+++ application/library/comm/Log.php
@@ -9,17 +9,50 @@
 
 class Comm_Log{
 
+    const
+        LEVEL_FATAL = 0,
+        LEVEL_NOTICE = 1;
 
-    public static function writeLog(){
+    const PREFIX = 'yake_';
 
+    /**
+     * 写日志
+     * @param int $level
+     * @param $msg
+     */
+    private static function writeLog(int $level, $msg){
+        $fileName = self::PREFIX . date('Y-m-d') . '.log';//按天划分
+        $path = APP_PATH . '/log/' . $fileName;
+        $time = date('Y-m-d H:i:s');
+        if (!is_string($msg)){
+            $msg = json_encode($msg);
+        }
+        $str = '';
+        switch ($level){
+            case self::LEVEL_NOTICE:
+                $str = '[' . $time . ']' . '[' . 'NOTICE' . ']'. $msg . PHP_EOL;
+                break;
+            case self::LEVEL_FATAL:
+                $str = '[' . $time . ']' . '[' . 'FATAL' . ']'. $msg . PHP_EOL;
+                break;
+        }
+        file_put_contents($path, $str, FILE_APPEND);
     }
 
-    public static function fatal(){
-        self::writeLog();
+    /**
+     * 严重错误日志
+     * @param $msg
+     */
+    public static function fatal($msg){
+        self::writeLog(self::LEVEL_FATAL, $msg);
     }
 
-    public static function notice(){
-        self::writeLog();
+    /**
+     * 一般错误日志
+     * @param $msg
+     */
+    public static function notice($msg){
+        self::writeLog(self::LEVEL_NOTICE, $msg);
     }
 
 }
