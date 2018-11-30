a129554d4d8131c0d62bb0f5cbaf1c9f4530c7e6
jiangbaiyan <987082332@qq.com>
2018-11-30 17:20:36 +0800

    配置文件重新上传

---
+++ application/config/Response.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 响应配置文件
+ * Created by PhpStorm.
+ * User: baiyan
+ * Date: 2018-11-28
+ * Time: 10:55
+ */
+
+return array(
+    'SUCCESS' => array(
+        'STATUS' => 200,
+        'MSG'    => 'success'
+    ),
+);
---
+++ application/config/Sms.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * 短信配置
+ * Created by PhpStorm.
+ * User: baiyan
+ * Date: 2018-11-28
+ * Time: 15:29
+ */
+
+return array(
+
+    'ali' => array(
+        'ACCESS_KEY_ID'     => '',
+        'ACCESS_KEY_SECRET' => '',
+        'SIGN_NAME'         => '',
+        'TEMPLATE_CODE'     => '',
+    )
+
+);
