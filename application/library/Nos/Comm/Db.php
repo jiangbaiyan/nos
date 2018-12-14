e8c9a8f370cb38c50b2194fe0040f1216743bea5
jiangbaiyan <987082332@qq.com>
2018-12-14 17:45:25 +0800

    修改Db类

--- application/library/Nos/Comm/Db.php
+++ application/library/Nos/Comm/Db.php
@@ -90,17 +90,19 @@ class Db{
             $handle = self::$db->prepare($sql);
             $res = $handle->execute($bind);
             if (!$res){
-               throw new CoreException(json_encode($handle->errorInfo()));
+                throw new CoreException(json_encode($handle->errorInfo()));
             }
+            $count = $handle->rowcount();
             if (self::$node == self::DB_NODE_MASTER_KEY){
-                return true;
+                return $count;
             }
-            $data = $handle->fetchAll(PDO::FETCH_ASSOC);
-            return $data;
+            if (!$count){
+                return array();
+            }
+            return $handle->fetchAll(PDO::FETCH_ASSOC);
         } catch (\Exception $e){
             Log::fatal('db|pdo_do_sql_failed|msg:' .  $e->getMessage() . '|sql:' . $sql . '|node:' . self::$node . '|bind:' . json_encode($bind));
             throw new CoreException('db query failed');
         }
     }
-
 }
