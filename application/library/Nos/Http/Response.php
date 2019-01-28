3c4adc28b6cd751722fe5ce7055f06b21d8c05de
jiangbaiyan <987082332@qq.com>
2019-01-28 10:57:30 +0800

    修复bug

--- application/library/Nos/Http/Response.php
+++ application/library/Nos/Http/Response.php
@@ -72,7 +72,7 @@ class Response{
      * @param string $msg
      * @return string
      */
-    public static function apiSuccess($data = [],$msg = ''){
+    public static function apiSuccess($data = array() ,$msg = ''){
         self::apiResponse(self::CODE_SUCCESS,$msg ? $msg :self::MSG_SUCCESS,$data);
     }
 
